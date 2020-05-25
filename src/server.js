1749524f13e81dda8d02f0681e543134ec97796f
Miguel Lopes <miguel.lopes@devoteam.com>
2020-05-25 12:29:34 +0200

    testing 123

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const httpRequestDurationMicroseconds = new Prometheus.Histogram({
   name: 'http_request_duration_ms',
   help: 'Duration of HTTP requests in ms',
   labelNames: ['method', 'route', 'code'],
-  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+  buckets: [50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
 })
 
 // editing extra stuff
